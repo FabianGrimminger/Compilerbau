35ce7e830f5d0db35129b6986d522617c6045bc5
Fabian Grimminger <grimmingerapps@gmail.com>
2017-05-22 10:41:25 +0200

    Empty LL1 Project

---
+++ ÜB-Praxis-LL1-Parser für X-Leer/src/de/dhbw/compiler/xparser/XParser.java
@@ -0,0 +1,25 @@
+/* **********************************************
+ * Duale Hochschule Baden-Württemberg Karlsruhe
+ * Prof. Dr. Jörn Eisenbiegler
+ * 
+ * Vorlesung Übersetzerbau
+ * Praxis LL(1)-Parser für X
+ * - LL1-Parser
+ * 
+ * **********************************************
+ */
+
+package de.dhbw.compiler.xparser;
+
+public class XParser {
+
+	public XParser(TokenBuffer in) {
+		//TODO Initialization
+	}
+
+	public Tree parseProgram() {
+		//TODO Parser
+		return null;
+	}
+
+}
